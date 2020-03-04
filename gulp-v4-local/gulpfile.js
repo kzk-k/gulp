4fc13e775ce008944f12aedcb0a13af713ac22c5
k.kawano <k-kawano@imt-mirai.co.jp>
2020-03-04 10:25:58 +0900

    インラインCSS

--- gulp-v4-local/gulpfile.js
+++ gulp-v4-local/gulpfile.js
@@ -2,26 +2,28 @@ const gulp				= require('gulp');
 const plumber			= require('gulp-plumber');
 const notify			= require('gulp-notify');
 const cache				= require('gulp-cached');
-const stylus			= require('gulp-stylus');
-const cssbeautify		= require('gulp-cssbeautify');
-const postcss			= require('gulp-postcss');
-const autoprefixer		= require('autoprefixer');
 // reload
 const connect			= require('gulp-connect-php');
-const browserSync		= require('browser-sync').create();
+const browserSync		= require('browser-sync');
+// css
+const stylus			= require('gulp-stylus');
+const postcss			= require('gulp-postcss');
+const autoprefixer		= require('autoprefixer');
+const cssbeautify		= require('gulp-cssbeautify');
+const minifycss			= require('gulp-clean-css');
 
-// const assets = './public/assets';
 const path = {
 	views: './*.php',
-	stylus: './stylus/',
+	stylus: './stylus/**/*.styl',
 	css: './css/',
+	critical: './stylus/**/critical.styl'
 }
 
-const postcssPlugin = [ autoprefixer() ];
+const postcssPlugin = [autoprefixer()];
 
 
 function stylusFunc() {
-	return gulp.src(path.stylus + '**/*.styl')
+	return gulp.src([path.stylus, '!' + path.critical])
 		.pipe(plumber({
 			errorHandler: notify.onError('Error: <%= error.message %>')
 		}))
@@ -35,15 +37,31 @@ function stylusFunc() {
 		.pipe(browserSync.stream());
 };
 
+function headCSS() {
+	return gulp.src(path.critical)
+		.pipe(plumber({
+			errorHandler: notify.onError('Error: <%= error.message %>')
+		}))
+		.pipe(cache(sass))
+		.pipe(sassGlob())
+		.pipe(sass())
+		.pipe(postcss(postcssPlugin))
+		.pipe(cssbeautify({
+			indent: '\t'
+		}))
+		// .pipe(minifycss())
+		.pipe(gulp.dest(path.css))
+		.pipe(browserSync.stream());
+};
 
 function connectSync() {
 	connect.server({
 		port: 8001
-		, base: '../test_gulp_v4/'
+		, base: '../gulp-v4-local/'
 	}, function() {
 		browserSync.init({
-			proxy: 'localhost:8001'
-			// , startPath: 'index.php'
+			proxy: 'localhost:8001/'
+			, startPath: 'index.php'
 			, ghostMode: false
 		});
 	});
@@ -56,9 +74,16 @@ function reload(done) {
 
 function watchFiles() {
 	gulp.watch(path.views, reload);
-	gulp.watch(path.stylus + '**/*.styl', stylusFunc);
+	gulp.watch(path.stylus, stylusFunc);
+	// gulp.watch(path.critical, headCSS);
 }
 
 
-const watch = gulp.parallel([watchFiles, connectSync]);
-exports.default = watch;
+// npx gulp
+exports.default = gulp.parallel([connectSync, watchFiles]);
+
+// 単独実行用（パーシャルファイルのコンパイル）
+exports.stylus = gulp.series([stylusFunc]);
+
+// 単独実行用（head内展開のCSSコンパイル）
+// exports.headCSS = gulp.series([headCSS]);
