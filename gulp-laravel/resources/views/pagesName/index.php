baf5396a9d28ed5c50037d0919b13ad11276ef89
k.kawano <k-kawano@imt-mirai.co.jp>
2020-08-07 17:09:41 +0900

    スカッシュテスト

---
+++ gulp-laravel/resources/views/pagesName/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width,initial-scale=1">
+	<title>gulp-laravel</title>
+	<meta name="format-detection" content="telephone=no">
+
+	<link rel="stylesheet" href="/public/css/pagesName/style.css">
+</head>
+<body>
+<?php include('../_layouts/_l-header.php'); ?>
+<main class="l-mainContainer">
+
+</main>
+</body>
+</html>
